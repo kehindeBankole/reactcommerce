42d6d63cceb792ca0da412899814dee3154e30f1
kehindeBankole <bankolek1@gmail.com>
2020-09-23 12:27:19 +0200

    added navbar

--- src/component/nav/Nav.js
+++ src/component/nav/Nav.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./nav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Nav() {
   return (
-    <div>
-      
-    </div>
-  )
+     <header>
+      <nav>
+        <div className="logo">
+          <p>KUMIA</p>
+        </div>
+        <div className="navtext">
+          <p>buy smarter , tested and trusted</p>
+        </div>
+        <div className="userpart">
+          <p>SIGN IN || REGISTRATION</p>
+        </div>
+        <div className="cart">
+          <p>
+            <FontAwesomeIcon icon={faCartPlus} />
+          </p>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Nav
+export default Nav;
