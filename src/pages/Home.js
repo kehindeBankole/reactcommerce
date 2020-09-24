f03f61fc152a77b770fe62552f04a4fca70ead60
kehindeBankole <bankolek1@gmail.com>
2020-09-24 13:19:27 +0100

    slider2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import Side from "../component/side/Side";
 import "./home.css";
 import slider1 from "../images/slider1.jpg";
+import slider2 from "../images/slider2.jpeg";
 import man from "../images/man.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 function Home() {
-  const [sliderArray] = useState([slider1]);
+  const [sliderArray] = useState([slider1 , slider2]);
   const sliderStyle = {
     backgroundImage: `url("${sliderArray[0]}")`,
     backgroundPosition: "center",
