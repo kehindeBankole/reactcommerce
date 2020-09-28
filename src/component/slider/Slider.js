32cd2f1423e643388c4322d6eac6a806906cf5db
kehindeBankole <bankolek1@gmail.com>
2020-09-28 12:42:51 +0100

    slider implemented

--- src/component/slider/Slider.js
+++ src/component/slider/Slider.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import SimpleImageSlider from "react-simple-image-slider"
+import React , { useState}  from 'react'
 import slider1 from "../../images/slider1.jpg";
 import slider2 from "../../images/slider2.jpeg";
 import slider3 from "../../images/slider3.jpg";
+import './slider.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 const Slider = () => {
-    const images = [
-        { url: slider1},
-        { url: slider2},
-        { url: slider3},
-    ];
+    const[current , setCurrent] = useState(0)
+  const images = [slider1, slider2, slider3]
+
+  function handleLeft() {
+    setCurrent(current===0? images.length-1 : current - 1)
+    console.log(current)
+  }
+  function handleRight() {
+    setCurrent(current===images.length-1? 0 : current + 1)
+  }
+
     return (
         <div>
-           <SimpleImageSlider
-                    width={896}
-                    height={504}
-                    images={images}
-                />
+          <div className="slider">
+          {
+            images.map((d,i)=>(
+              <div className={i===current?`slide active`:`slide`}>
+                <img src={d} alt="" id="sliderimage"/>
+              </div>
+            ))
+          }
+          <button className="btn" id="left" onClick={handleLeft}>
+            <FontAwesomeIcon icon={faCaretLeft} />
+          </button>
+          <button className="btn" id="right" onClick={handleRight}>
+            <FontAwesomeIcon icon={faCaretRight} />
+          </button>
+        </div>
         </div>
     )
 }
