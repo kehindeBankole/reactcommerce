54e8f694e6bbc2e0e77e991904629eed2a7cbf1e
kehindeBankole <bankolek1@gmail.com>
2020-09-30 08:53:39 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "../src/pages/Home";
+import ProductView from "./pages/ProductView"
 import Nav from "./component/nav/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Productstate from "./context/product/state";
@@ -11,6 +12,7 @@ function App() {
       <Nav />
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/product/:id" component={ProductView} exact />
       </Switch>
     </Router>
 </Productstate>
